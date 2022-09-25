22e45aeb24c6be9759fbade11b2bf04c3610dceb
lishijiee <2424782003@qq.com>
2022-09-25 17:11:50 +0800

    Initial commit

---
+++ src/main/java/com/lishijiee/DemosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.lishijiee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemosSpringBootApplication.class, args);
+    }
+
+}
