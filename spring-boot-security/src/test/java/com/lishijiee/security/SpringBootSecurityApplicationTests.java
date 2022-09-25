cb67c451a044f6654909644fc6229c471019bb50
lishijiee <2424782003@qq.com>
2022-09-25 23:30:11 +0800

    security-

---
+++ spring-boot-security/src/test/java/com/lishijiee/security/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lishijiee.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
