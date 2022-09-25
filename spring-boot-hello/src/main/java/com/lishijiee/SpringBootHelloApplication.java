b4deafc1830ab0c5f39dd9363b8da249e23bdf66
lishijiee <2424782003@qq.com>
2022-09-25 18:47:38 +0800

    spring-boot-hello 测试

--- spring-boot-hello/src/main/java/com/lishijiee/SpringBootHelloApplication.java
+++ spring-boot-hello/src/main/java/com/lishijiee/SpringBootHelloApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemosSpringBootApplication {
+public class SpringBootHelloApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemosSpringBootApplication.class, args);
+        SpringApplication.run(SpringBootHelloApplication.class, args);
     }
 
 }
